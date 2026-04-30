e81a30e7e7731510af94fb5ecefea9157321b3be
Mark Holtzhausen <nemesarial@gmail.com>
2026-04-30 10:22:25 +0200

    task(2.7): re-export all primitives from prelude
    Update components/src/prelude.rs to re-export all primitives via
     so users can do
     and get access to Card, Button,
    Toggle, Badge, Avatar, ButtonAction, etc. in one glob import.

--- components/src/prelude.rs
+++ components/src/prelude.rs
@@ -3,3 +3,7 @@
 //! ```rust
 //! use relm4_kit::prelude::*;
 //! ```
+
+// Re-export all primitives so users can do `use relm4_kit::prelude::*;`
+// and get Card, Button, Toggle, Badge, Avatar, ButtonAction, etc.
+pub use crate::primitives::*;
